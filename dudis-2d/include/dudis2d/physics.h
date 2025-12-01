7d0666be26cb0f9f7a5a880f550d3797b4d0d413
Math3uso <matheusfred955@gmail.com>
2025-12-01 01:01:11 -0300

    refactor: update includes in physics.h to new paths

--- dudis-2d/include/dudis2d/physics.h
+++ dudis-2d/include/dudis2d/physics.h
@@ -1,6 +1,11 @@
 #pragma once
 
-#include "dudis2d/core/physicsComponent/body/body.h"
-#include "dudis2d/core/physicsComponent/contactListenear/contactListenear.h"
-#include "dudis2d/core/physicsComponent/debugDraw/debugDraw.h"
-#include "dudis2d/core/physicsComponent/physicsComponent.h"
+// #include "dudis2d/core/physicsComponent/body/body.h"
+// #include "dudis2d/core/physicsComponent/contactListenear/contactListenear.h"
+// #include "dudis2d/core/physicsComponent/debugDraw/debugDraw.h"
+// #include "dudis2d/core/physicsComponent/physicsComponent.h"
+
+#include "dudis2d/core/component/physicsComponent/body/body.h"
+#include "dudis2d/core/component/physicsComponent/contactListenear/contactListenear.h"
+#include "dudis2d/core/component/physicsComponent/debugDraw/debugDraw.h"
+#include "dudis2d/core/component/physicsComponent/physicsComponent.h"
