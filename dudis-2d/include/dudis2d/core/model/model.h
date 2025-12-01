c4e9781398f94fcfa781e00b2c0d5b11893c8bd4
Math3uso <matheusfred955@gmail.com>
2025-12-01 01:10:45 -0300

    deprecate: mark Model class as obsolete for future removal

--- dudis-2d/include/dudis2d/core/model/model.h
+++ dudis-2d/include/dudis2d/core/model/model.h
@@ -1,102 +1,103 @@
-#pragma once
+// #pragma once
 
-#include "dudis2d/core/physicsComponent/physicsComponent.h"
-#include "dudis2d/core/utils/sizeI.h"
-#include "dudis2d/core/utils/types.h"
-#include "dudis2d/graphics/renderable.h"
-#include "dudis2d/graphics/utils/animatedList/animatedList.h"
-#include "dudis2d/scenes/scene/scene.h"
-#include "dudis2d/scenes/sceneProperty/sceneProperty.h"
-#include <memory>
+// #include "dudis2d/core/physicsComponent/physicsComponent.h"
+// #include "dudis2d/core/utils/sizeI.h"
+// #include "dudis2d/core/utils/types.h"
+// #include "dudis2d/graphics/renderable.h"
+// #include "dudis2d/graphics/utils/animatedList/animatedList.h"
+// #include "dudis2d/scenes/scene/scene.h"
+// #include "dudis2d/scenes/sceneProperty/sceneProperty.h"
+// #include <memory>
 
-class Scene;
+// class Scene;
 
-namespace dudis {
-class DDModel {
-private:
-  // std::shared_ptr<AnimatedList> animated;
+// namespace dudis {
+// class DDModel {
+// private:
+//   // std::shared_ptr<AnimatedList> animated;
 
-protected:
-  bool flippedX;
-  Vec2 pos{0, 0};
-  float angle{0.0f};
-  Vec2 origin = {0, 0};
-  dudis::SizeI size{0, 0};
-  std::string name{""};
-  SceneProperty sceneProps;
+// protected:
+//   bool flippedX;
+//   Vec2 pos{0, 0};
+//   float angle{0.0f};
+//   Vec2 origin = {0, 0};
+//   dudis::SizeI size{0, 0};
+//   std::string name{""};
+//   SceneProperty sceneProps;
 
-  std::shared_ptr<Renderable> renderable = nullptr;
-  std::shared_ptr<PhysicsComponent> physicsComponent = nullptr;
-  std::shared_ptr<AnimatedList> animated = nullptr;
+//   std::shared_ptr<Renderable> renderable = nullptr;
+//   std::shared_ptr<PhysicsComponent> physicsComponent = nullptr;
+//   std::shared_ptr<AnimatedList> animated = nullptr;
 
-public:
-  DDModel() : pos{0, 0}, angle(0.0f), size{0, 0}, name("") {}
-  virtual ~DDModel();
+// public:
+//   DDModel() : pos{0, 0}, angle(0.0f), size{0, 0}, name("") {}
+//   virtual ~DDModel();
 
-  virtual void start() {};
-  virtual void update() {
-    if (physicsComponent && physicsComponent->hasBody()) {
-      physicsComponent->syncFromPhysics();
-    }
-  };
+//   virtual void start() {};
+//   virtual void update() {
+//     if (physicsComponent && physicsComponent->hasBody()) {
+//       physicsComponent->syncFromPhysics();
+//     }
+//   };
 
-  virtual void onCheckCollider() {}
+//   virtual void onCheckCollider() {}
 
-  virtual void release();
+//   virtual void release();
 
-  virtual void onDestroyer() {
-    sceneProps.onDelete = true;
-    this->renderable->getSceneProps().onDelete = true;
-  }
+//   virtual void onDestroyer() {
+//     sceneProps.onDelete = true;
+//     this->renderable->getSceneProps().onDelete = true;
+//   }
 
-  template <typename T> void addComponent(std::shared_ptr<T> component);
-  template <typename T> static std::shared_ptr<T> create();
+//   template <typename T> void addComponent(std::shared_ptr<T> component);
+//   template <typename T> static std::shared_ptr<T> create();
 
-  PhysicsComponent *getPhysicsComponent() { return physicsComponent.get(); }
+//   PhysicsComponent *getPhysicsComponent() { return physicsComponent.get(); }
 
-  const SceneProperty &getSceneProps() const { return sceneProps; }
+//   const SceneProperty &getSceneProps() const { return sceneProps; }
 
-  std::shared_ptr<Renderable> &getRenderable();
-  void render();
-  void move(Vec2 delta);
-  void setFlippedX(bool flip) { flippedX = flip; }
+//   std::shared_ptr<Renderable> &getRenderable();
+//   void render();
+//   void move(Vec2 delta);
+//   void setFlippedX(bool flip) { flippedX = flip; }
 
-  Vec2 getMovementInput();
-  Vec2 &getPos() { return pos; }
-  const SizeI &getSize() const { return size; }
-  const float &getAngle() { return angle; }
-  void setAngle(const float &nAngle) { angle = nAngle; }
-  void setPos(const Vec2 &nPos) { pos = nPos; }
-  void setSize(const SizeI &nSize) { size = nSize; }
+//   Vec2 getMovementInput();
+//   Vec2 &getPos() { return pos; }
+//   const SizeI &getSize() const { return size; }
+//   const float &getAngle() { return angle; }
+//   void setAngle(const float &nAngle) { angle = nAngle; }
+//   void setPos(const Vec2 &nPos) { pos = nPos; }
+//   void setSize(const SizeI &nSize) { size = nSize; }
 
-  AnimatedList *getAnimated() { return animated.get(); }
+//   AnimatedList *getAnimated() { return animated.get(); }
 
-  // const char *getName() { return name; }
+//   // const char *getName() { return name; }
 
-  const std::string &getName() const { return name; }
+//   const std::string &getName() const { return name; }
 
-  static std::shared_ptr<DDModel> create();
+//   static std::shared_ptr<DDModel> create();
 
-  friend class PhysicsComponent;
-};
+//   friend class PhysicsComponent;
+// };
 
-template <typename T> void DDModel::addComponent(std::shared_ptr<T> component) {
-  std::cout << "verificando de if passa\n";
-  if constexpr (std::is_same<T, Renderable>::value) {
-    this->renderable = component;
-    std::cout << "if passou Renderable\n";
-    this->renderable->setSize({size.w, size.h});
-  } else if constexpr (std::is_same<T, PhysicsComponent>::value) {
-    this->physicsComponent = component;
-    std::cout << "if passou - PhysicsComponent\n";
-  } else if constexpr (std::is_same<T, AnimatedList>::value) {
-    this->animated = component;
-    std::cout << this->animated->Animations().size() << "\n";
-    std::cout << "if passou - AnimatedLists\n";
-  }
-}
-template <typename T> std::shared_ptr<T> DDModel::create() {
-  return std::make_shared<T>();
-}
+// template <typename T> void DDModel::addComponent(std::shared_ptr<T>
+// component) {
+//   std::cout << "verificando de if passa\n";
+//   if constexpr (std::is_same<T, Renderable>::value) {
+//     this->renderable = component;
+//     std::cout << "if passou Renderable\n";
+//     this->renderable->setSize({size.w, size.h});
+//   } else if constexpr (std::is_same<T, PhysicsComponent>::value) {
+//     this->physicsComponent = component;
+//     std::cout << "if passou - PhysicsComponent\n";
+//   } else if constexpr (std::is_same<T, AnimatedList>::value) {
+//     this->animated = component;
+//     std::cout << this->animated->Animations().size() << "\n";
+//     std::cout << "if passou - AnimatedLists\n";
+//   }
+// }
+// template <typename T> std::shared_ptr<T> DDModel::create() {
+//   return std::make_shared<T>();
+// }
 
-} // namespace dudis
+// } // namespace dudis
--- dudis-2d/src/core/model/model.cpp
+++ dudis-2d/src/core/model/model.cpp
@@ -1,93 +1,93 @@
-#include "dudis2d/core/model/model.h"
-#include "dudis2d/dudis2d.h"
-#include <type_traits>
+// #include "dudis2d/core/model/model.h"
+// #include "dudis2d/dudis2d.h"
+// #include <type_traits>
 
-using namespace dudis;
-using namespace std;
+// using namespace dudis;
+// using namespace std;
 
-void DDModel::render()
-{
+// void DDModel::render()
+// {
 
-  if (animated)
-  {
-    this->renderable = animated->getCurrentAnimation();
-  }
+//   if (animated)
+//   {
+//     this->renderable = animated->getCurrentAnimation();
+//   }
 
-  if (renderable)
-  {
-    renderable->setPos({pos.x, pos.y});
-    renderable->rotate(angle);
-    renderable->setOrigin(origin);
+//   if (renderable)
+//   {
+//     renderable->setPos({pos.x, pos.y});
+//     renderable->rotate(angle);
+//     renderable->setOrigin(origin);
 
-    this->renderable->setSize({size.w, size.h});
+//     this->renderable->setSize({size.w, size.h});
 
-    this->renderable->setFlippedX(flippedX);
+//     this->renderable->setFlippedX(flippedX);
 
-    // this->renderable->render();
-  }
-}
+//     // this->renderable->render();
+//   }
+// }
 
-shared_ptr<Renderable> &DDModel::getRenderable()
-{
-  return this->renderable;
-}
+// shared_ptr<Renderable> &DDModel::getRenderable()
+// {
+//   return this->renderable;
+// }
 
-DDModel::~DDModel()
-{
-  if (!name.empty())
-  {
-    std::cout << "model " << name << " deletado\n";
-  }
-  else
-  {
-    std::cout << "model sem nome deletado\n";
-  }
-}
+// DDModel::~DDModel()
+// {
+//   if (!name.empty())
+//   {
+//     std::cout << "model " << name << " deletado\n";
+//   }
+//   else
+//   {
+//     std::cout << "model sem nome deletado\n";
+//   }
+// }
 
-Vec2 DDModel::getMovementInput()
-{
+// Vec2 DDModel::getMovementInput()
+// {
 
-  Vec2 input{0, 0};
+//   Vec2 input{0, 0};
 
-  if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT))
-    input.x -= 1;
-  if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT))
-    input.x += 1;
-  if (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP))
-    input.y -= 1;
-  if (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN))
-    input.y += 1;
+//   if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT))
+//     input.x -= 1;
+//   if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT))
+//     input.x += 1;
+//   if (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP))
+//     input.y -= 1;
+//   if (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN))
+//     input.y += 1;
 
-  return input;
-}
+//   return input;
+// }
 
-void DDModel::move(Vec2 delta)
-{
-  pos = delta;
-}
+// void DDModel::move(Vec2 delta)
+// {
+//   pos = delta;
+// }
 
-void DDModel::release()
-{
+// void DDModel::release()
+// {
 
-  // if (this->renderable) {
-  //   this->renderable.reset();
-  //   Log::Success("Renderable liberado");
-  // }
-  // else if (this->animated) {
-  //   this->animated.reset();
-  //   Log::Success("Animated liberado");
-  // }
+//   // if (this->renderable) {
+//   //   this->renderable.reset();
+//   //   Log::Success("Renderable liberado");
+//   // }
+//   // else if (this->animated) {
+//   //   this->animated.reset();
+//   //   Log::Success("Animated liberado");
+//   // }
 
-  cout << "total de referencias: " << this->renderable.use_count() << "\n";
+//   cout << "total de referencias: " << this->renderable.use_count() << "\n";
 
-  this->renderable.reset();
-  this->renderable = nullptr;
+//   this->renderable.reset();
+//   this->renderable = nullptr;
 
-  cout << "total de referencias: " << this->animated.use_count() << "\n";
+//   cout << "total de referencias: " << this->animated.use_count() << "\n";
 
-  Log::Success("Renderable liberado");
+//   Log::Success("Renderable liberado");
 
-  this->animated.reset();
-  Log::Success("Animated liberado");
-  Log::Success("Recursos de DDModel: " + name + " liberados");
-}
+//   this->animated.reset();
+//   Log::Success("Animated liberado");
+//   Log::Success("Recursos de DDModel: " + name + " liberados");
+// }
