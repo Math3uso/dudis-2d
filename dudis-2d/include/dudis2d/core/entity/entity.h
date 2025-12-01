57cf561ae80e2813fc7b240cd59a1cea6ea5382e
Math3uso <matheusfred955@gmail.com>
2025-12-01 00:20:33 -0300

    Refactor/Feature: rewrite Entity class with new features
    - Major refactor of Entity class (entity.h and entity.cpp)
    - Redesigned core structure to support parent-child hierarchy more robustly
    - Added global/local transform caching, improved Z-order handling
    - Enhanced scale, rotation, and position management (local and global)
    - Added utility methods for children management and scene integration

--- dudis-2d/include/dudis2d/core/entity/entity.h
+++ dudis-2d/include/dudis2d/core/entity/entity.h
@@ -1,45 +1,188 @@
 #pragma once
 
+#include "dudis2d/core/component/physicsComponent/physicsComponent.h"
 #include "dudis2d/core/entity/entity.h"
 #include "dudis2d/core/utils/types.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/string_cast.hpp>
+#include <memory>
+#include <typeindex>
+#include <unordered_map>
 
 namespace dudis {
 
 class Motion;
+class Component;
+// class PhysicsComponent;
+
+/**
+ * @brief Tipo de transformação de uma entidade em relação ao seu pai.
+ *
+ * Define como a transformação local de uma entidade se combina com a
+ * transformação do seu elemento pai.
+ */
+enum class TransformType {
+  /**
+   * @brief Transformação relativa ao pai (padrão).
+   *
+   * O filho segue todas as transformações do pai. Ou seja,
+   * posição, rotação e escala do pai influenciam diretamente no filho.
+   */
+  Relative,
+
+  /**
+   * @brief Transformação absoluta.
+   *
+   * A entidade ignora as transformações do pai e usa apenas sua
+   * posição, rotação e escala locais. Somente esta entidade é afetada.
+   */
+  Absolute,
+
+  /**
+   * @brief Ignora transformações do pai para todos os filhos.
+   *
+   * Se um pai tiver este tipo, todos os seus filhos passam a ser
+   * tratados como Absolute, ou seja, não recebem nenhuma
+   * influência da transformação do pai.
+   */
+  DisableParentTransform
+};
 
 class Entity {
 
+private:
+  bool _localDirty = false;
+  bool _globalDirty = true;
+  bool _orderChildren = false;
+  glm::mat4 _localMatrix = glm::mat4(1.0f);
+  glm::mat4 _globalMatrix = glm::mat4(1.0f);
+  std::unordered_map<std::type_index, std::shared_ptr<Component>> _components;
+  bool _physicsComponent = false;
+  TransformType _transformType = TransformType::Relative;
+  int _zOrder = 0;
+
 protected:
-  dudis::SizeI size;
-  dudis::Vec2 pos;
+  dudis::SizeI size = {0, 0};
+  dudis::Vec2 pos = {0, 0};
+  Vec2 origin = {0, 0};
+  Vec2 scale = {1, 1};
   float angle = 0.0f; // in radians
   std::vector<std::shared_ptr<Motion>> actions;
   std::shared_ptr<Motion> _action;
+  std::vector<std::shared_ptr<Entity>> _children;
+  Entity *_parent = nullptr;
+  void _sortChildrenByIndex();
 
 public:
+  Entity() = default;
   float getAngle() const { return angle; }
   Vec2 getPos() const { return pos; }
   SizeI getSize() const { return size; }
+  Vec2 getOrigin() const { return origin; }
 
-  virtual void setPos(dudis::Vec2 nPos) { pos = nPos; }
-  virtual void setSize(dudis::SizeI nSize) { size = nSize; }
-  virtual void translate(dudis::Vec2 nPos) {
-    pos.x += nPos.x;
-    pos.y += nPos.y;
-  }
+  const Vec2 getGlobalPos();
+  float getGlobalRotation();
+  const Vec2 getGlocalScale();
+  Size getGlobalSize();
 
-  virtual void rotate(float nAngle) { angle = nAngle; }
+  void setTransformType(TransformType type) { _transformType = type; }
+
+  virtual void setPos(const dudis::Vec2 &nPos);
+  void setPos(float x, float y);
+  void setZOrder(int zIndex);
+
+  virtual void setSize(dudis::SizeI nSize);
+  virtual void translate(const dudis::Vec2 &nPos);
+
+  virtual void rotate(float nAngle);
   virtual void update() {};
+  virtual void render();
+  virtual void updateChildren();
 
   void defaultUpdate();
-
   void runMotions();
+  void setDirty();
 
+  const glm::mat4 &getLocalMatrix();
+  const glm::mat4 &getGlobalMatrix();
+
+  void setParent(Entity *parent) { _parent = parent; };
+  void setOrigin(Vec2 nOrigin) { origin = nOrigin; }
   void setMotion(std::shared_ptr<Motion> action) {
     // this->actions.push_back(action);
     _action = action;
   };
 
-  virtual ~Entity() = default;
+  void addChild(std::shared_ptr<Entity> entity);
+  void addChild(std::shared_ptr<Entity> entity, const int zOrder);
+
+  const std::vector<std::shared_ptr<Entity>> &getChildren() {
+    //_sortChildrenByIndex();
+    return _children;
+  };
+
+  template <typename T, typename... Args>
+  std::shared_ptr<T> addComponent(Args &&...args) {
+    auto comp = std::make_shared<T>(std::forward<Args>(args)...);
+    _components[typeid(T)] = comp;
+
+    if (auto phys = std::dynamic_pointer_cast<PhysicsComponent>(comp)) {
+      _physicsComponent = true;
+      puts("compoenent fisico enviado");
+    }
+
+    return comp;
+  }
+
+  // versão para usar instância já criada
+  template <typename T>
+  std::shared_ptr<T> addComponent(std::shared_ptr<T> &comp) {
+    _components[typeid(T)] = comp;
+
+    if (auto phys = std::dynamic_pointer_cast<PhysicsComponent>(comp)) {
+      _physicsComponent = true;
+      puts("component fisico enviado [INSTANCIA]");
+    }
+
+    return comp;
+  }
+
+  // Versão genérica
+  // template <typename T, typename... Args>
+  // std::shared_ptr<T> addComponent(Args &&...args) {
+  //   std::shared_ptr<T> comp;
+
+  //   if constexpr (std::is_same_v<T, PhysicsComponent>) {
+  //     // usa a factory padrão de box physics
+  //     comp = PhysicsComponent::createBoxPhysics(this);
+  //     _physicsComponent = true;
+  //     puts("compoenete de fisica criado");
+  //   } else {
+  //     comp = std::make_shared<T>(std::forward<Args>(args)...);
+  //   }
+
+  //   _components[typeid(T)] = comp;
+
+  //   if (auto phys = std::dynamic_pointer_cast<PhysicsComponent>(comp)) {
+  //     _physicsComponent = true;
+  //     puts("compoenete de fisica criado");
+  //   }
+
+  //   return comp;
+  // }
+
+  template <typename T> std::shared_ptr<T> getComponent() {
+    auto it = _components.find(typeid(T));
+    if (it != _components.end())
+      return std::static_pointer_cast<T>(it->second);
+    return nullptr;
+  }
+
+  template <typename T> void removeComponent() { _components.erase(typeid(T)); }
+
+  // void release();
+
+  virtual ~Entity();
 };
 } // namespace dudis
--- dudis-2d/src/core/entity/entity.cpp
+++ dudis-2d/src/core/entity/entity.cpp
@@ -1,10 +1,18 @@
 #include "dudis2d/core/entity/entity.h"
+#include "dudis2d/core/log/log.h"
 #include "dudis2d/core/motion/motion.h"
+#include <algorithm>
 
+using namespace std;
 using namespace dudis;
 
 void Entity::defaultUpdate() {
   this->update();
+  if (_physicsComponent) {
+    auto physicsComponent = this->getComponent<PhysicsComponent>();
+    physicsComponent->syncFromPhysics();
+  }
+  // this->updateChildren();
   this->runMotions();
 }
 
@@ -20,4 +28,188 @@ void Entity::runMotions() {
   // if (_action && !_action->isDone()) {
   //   _action->step();
   // }
+}
+
+void Entity::updateChildren() {}
+
+void Entity::render() { this->getGlobalMatrix(); }
+
+void Entity::addChild(shared_ptr<Entity> child) {
+
+  if (_transformType == TransformType::DisableParentTransform) {
+    child->setTransformType(TransformType::Absolute);
+  }
+
+  if (child->_zOrder == 0) {
+    child->_zOrder = _children.size() + 1;
+  }
+
+  this->setDirty();
+  child->setParent(this);
+  _children.push_back(child);
+  _orderChildren = true;
+  this->_sortChildrenByIndex();
+}
+
+void Entity::addChild(shared_ptr<Entity> child, const int zOrder) {
+  child->_zOrder = zOrder;
+  this->addChild(child);
+  return;
+}
+
+/**
+ * @brief Calcula e retorna a matriz de transformação local do Entity.
+ *
+ * Esta matriz representa a posição, rotação e escala do Entity
+ * relativas ao seu elemento pai (se houver).
+ *
+ * A matriz é recalculada apenas quando a posição, rotação, escala
+ * do Entity ou do seu pai é modificada, evitando cálculos desnecessários.
+ *
+ * Transformações aplicadas na ordem:
+ * 1. Translação (posição)
+ * 2. Rotação (ângulo em torno do eixo Z)
+ * 3. Escala (dimensões local X e Y)
+ *
+ * @return const glm::mat4& Referência constante à matriz de transformação
+ * local.
+ */
+const glm::mat4 &Entity::getLocalMatrix() {
+  if (_localDirty) {
+    _localMatrix = glm::mat4(1.0f);
+    _localMatrix = glm::translate(_localMatrix, glm::vec3(pos.x, pos.y, 0));
+    _localMatrix = glm::rotate(_localMatrix, angle, glm::vec3(0, 0, 1));
+    _localMatrix = glm::scale(_localMatrix, glm::vec3(scale.x, scale.y, 1));
+
+    _localDirty = false;
+  }
+
+  return _localMatrix;
+}
+
+/**
+ * @brief Calcula e retorna a matriz de transformação global do Entity.
+ *
+ * Esta matriz representa a transformação completa do Entity no espaço mundial,
+ * considerando a hierarquia de pais e o tipo de transformação.
+ *
+ * - Se o Entity tiver um pai e seu tipo de transformação não for Absolute,
+ *   a matriz global é calculada como: matrizGlobalPai * matrizLocal.
+ * - Caso contrário, a matriz global é igual à matriz local.
+ *
+ * A matriz global é recalculada apenas quando o Entity ou algum de seus pais
+ * é modificado (_globalDirty é true), evitando cálculos desnecessários.
+ *
+ * @return const glm::mat4& Referência constante à matriz de transformação
+ * global.
+ */
+const glm::mat4 &Entity::getGlobalMatrix() {
+  if (_globalDirty) {
+    if (_parent && _transformType != TransformType::Absolute) {
+      _globalMatrix = _parent->getGlobalMatrix() * getLocalMatrix();
+    } else {
+      _globalMatrix = getLocalMatrix();
+    }
+    _globalDirty = false;
+  }
+  return _globalMatrix;
+}
+
+void Entity::setPos(const dudis::Vec2 &nPos) {
+  this->setDirty();
+  pos = nPos;
+}
+
+void Entity::setPos(float x, float y) {
+  this->setPos(Vec2{x, y});
+  return;
+}
+
+void Entity::setSize(dudis::Size nSize) {
+  this->setDirty();
+  size = nSize;
+}
+
+void Entity::setZOrder(int zIndex) {
+  _zOrder = zIndex;
+  // temp entity precia de um zOrderGlobal
+  _parent->_orderChildren = true;
+  _parent->_sortChildrenByIndex();
+}
+
+void Entity::rotate(float nAngle) {
+  this->setDirty();
+  angle = nAngle;
+}
+
+void Entity::translate(const Vec2 &nPos) {
+
+  this->setDirty();
+  pos.x += nPos.x;
+  pos.y += nPos.y;
+}
+
+void Entity::setDirty() {
+  _localDirty = true;
+  _globalDirty = true;
+  for (auto &child : _children) {
+    child->setDirty();
+  }
+}
+
+const Vec2 Entity::getGlobalPos() {
+  glm::mat4 m = getGlobalMatrix();
+  return {m[3][0], m[3][1]};
+}
+
+float Entity::getGlobalRotation() {
+  glm::mat4 m = getGlobalMatrix();
+
+  float scaleX = glm::length(glm::vec3(m[0]));
+  float scaleY = glm::length(glm::vec3(m[1]));
+
+  float rot = atan2(m[1][0] / scaleY, m[0][0] / scaleX);
+
+  return rot;
+}
+
+const Vec2 Entity::getGlocalScale() {
+  glm::mat4 m = getGlobalMatrix();
+  float scaleX = glm::length(glm::vec3(m[0])); // 1ª coluna
+  float scaleY = glm::length(glm::vec3(m[1])); // 2ª coluna
+  return {scaleX, scaleY};
+}
+
+Size Entity::getGlobalSize() {
+
+  glm::mat4 m = getGlobalMatrix();
+
+  float scaleX = glm::length(glm::vec3(m[0])); // coluna X
+  float scaleY = glm::length(glm::vec3(m[1])); // coluna Y
+
+  return {size.w * scaleX, size.h * scaleY};
+}
+
+// void Entity::release() { _children.clear(); }
+
+void Entity::_sortChildrenByIndex() {
+  //_children
+  if (_orderChildren) {
+    Log::Info("order chamado");
+    sort(
+        _children.begin(), _children.end(),
+        [](const shared_ptr<Entity> &child1, const shared_ptr<Entity> &child2) {
+          return child1->_zOrder < child2->_zOrder;
+        });
+    _orderChildren = false;
+  }
+  return;
+}
+
+Entity::~Entity() {
+
+  Log::Info("Entity liberada [ENTITY]");
+
+  _components.clear();
+  _children.clear();
 }
