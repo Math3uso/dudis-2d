08627146d8dbf5a110c398dce804ab8fc1e5b1d5
Math3uso <matheusfred955@gmail.com>
2026-02-18 20:04:31 -0300

    input: map extra mouse buttons

--- dudis-2d/src/core/ddrlUtils/toRLMouseButton.cpp
+++ dudis-2d/src/core/ddrlUtils/toRLMouseButton.cpp
@@ -17,7 +17,21 @@ namespace dudis
 
             case MouseButton::Middle:
                 return MOUSE_BUTTON_MIDDLE;
+
+            case MouseButton::Side:
+                return MOUSE_BUTTON_SIDE;
+
+            case MouseButton::Extra:
+                return MOUSE_BUTTON_EXTRA;
+
+            case MouseButton::Forward:
+                return MOUSE_BUTTON_FORWARD;
+
+            case MouseButton::Back:
+                return MOUSE_BUTTON_BACK;
             }
+
+            return -1;
         }
     }
 }
