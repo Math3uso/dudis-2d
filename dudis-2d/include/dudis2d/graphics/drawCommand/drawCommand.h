07f1b5dca8447b1446cf82b72375394eb7722532
Math3uso <matheusfred955@gmail.com>
2026-02-18 20:04:31 -0300

    graphics: extend DrawCommand for textures

--- dudis-2d/include/dudis2d/graphics/drawCommand/drawCommand.h
+++ dudis-2d/include/dudis2d/graphics/drawCommand/drawCommand.h
@@ -1,21 +1,27 @@
 #pragma once
 
 #include "dudis2d/core/utils/types.h"
+#include "dudis2d/graphics/ddAssets/texture2D.h"
 
-namespace dudis {
+namespace dudis
+{
 
-class DrawCommand {
-public:
-  Vec2 pos;
-  Vec2 scale;
-  Size size;
-  float rotation;
-  Vec2 origin;
-  uint32_t tex;
-  DDRect src;
-  Color color;
-  DDPrimitiveType type;
-  int z;
-};
+  class DrawCommand
+  {
+  public:
+    Vec2 pos;
+    Vec2 scale;
+    SizeF size;
+    float rotation;
+    Vec2 origin;
+    bool _tex;
+    rl::RlTexture rlTex;
+    uint32_t texId;
+    DDRect src;
+    Color color;
+    DDPrimitiveType type;
+    DDBatchType batch;
+    int z;
+  };
 
 } // namespace dudis
