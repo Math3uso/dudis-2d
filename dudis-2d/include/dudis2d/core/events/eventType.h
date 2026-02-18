4904bbba170567b06f8bbe64fb856a5e8773d621
Math3uso <matheusfred955@gmail.com>
2026-02-18 20:04:32 -0300

    events: add event enums

---
+++ dudis-2d/include/dudis2d/core/events/eventType.h
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace dudis
+{
+    enum class ListenOnly
+    {
+        Window,
+        Keyboard,
+        Mouse,
+        KeyUp,
+        KeyDown,
+        KeyPress,
+    };
+
+    enum class Events
+    {
+        Window,
+        Keyboard,
+        Mouse
+    };
+}
