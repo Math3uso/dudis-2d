9261fb7845c141099de506e161ac10aa3e08a2d8
Math3uso <matheusfred955@gmail.com>
2026-02-18 20:04:31 -0300

    graphics: move Renderable to command-based rendering

--- dudis-2d/include/dudis2d/graphics/renderable.h
+++ dudis-2d/include/dudis2d/graphics/renderable.h
@@ -2,53 +2,70 @@
 
 #include "dudis2d/core/entity/entity.h"
 #include "dudis2d/core/utils/types.h"
+#include "dudis2d/graphics/ddAssets/texture2D.h"
 #include "dudis2d/scenes/sceneProperty/sceneProperty.h"
 
-namespace dudis {
+namespace dudis
+{
 
-enum class TypeShape { Triangle, Rectangle, Circle };
-
-class Renderable : public Entity {
-protected:
-  // Vec2 origin = {0, 0};
-  dudis::TypeShape type;
-  Color color = WHITE;
-  bool flippedX = false;
-  SceneProperty sceneProps;
-  std::string filePath;
-
-public:
-  int zIndex = -1;
-  bool drawOrigin = false;
-  const char *name;
-
-  Renderable() {};
-  virtual ~Renderable() { puts("renderable deletado [Renderable]"); };
-  void render() override = 0;
-  void update() override {};
-
-  // const std::string &getFilePath() const {
-  //   return filePath;
-  // }
-
-  // Vec2 getOrigin() const { return origin; }
-  Color getColor() const { return color; }
-  dudis::TypeShape Type() const { return type; }
-
-  SceneProperty &getSceneProps() { return sceneProps; }
-
-  // void rotate(float nAngle) { angle = nAngle; }
-  //  void setOrigin(Vec2 nOrigin) { origin = nOrigin; }
-  void setColor(Color nColor) { color = nColor; }
-
-  virtual void setFlippedX(bool flip) {
-    this->setDirty();
-    flippedX = flip;
+  enum class TypeShape
+  {
+    Triangle,
+    Rectangle,
+    Circle
   };
 
-  void setOpacity(float opacity) {
-    this->setDirty();
-    color = {color.r, color.g, color.b, (unsigned char)opacity};
-  }
-};
+  class Renderable : public Entity
+  {
+  protected:
+    // Vec2 origin = {0, 0};
+    dudis::TypeShape type;
+    Color color = WHITE;
+    bool flippedX = false;
+    SceneProperty sceneProps;
+    std::string filePath;
+    DDRect _rectSrc;
+    DDRect _rectDest;
+    bool _tex = false;
+    rl::RlTexture *_rlTex;
+    res::DDTexture _ddTex;
+
+  public:
+    int zIndex = -1;
+    bool drawOrigin = false;
+    const char *name;
+
+    Renderable() {};
+    virtual ~Renderable() { puts("renderable deletado [Renderable]"); };
+    void render() override = 0;
+    void update() override {};
+
+    virtual void buildRenderCommands(RenderQueue *queue) override;
+
+    // const std::string &getFilePath() const {
+    //   return filePath;
+    // }
+
+    // Vec2 getOrigin() const { return origin; }
+    Color getColor() const { return color; }
+    dudis::TypeShape Type() const { return type; }
+
+    SceneProperty &getSceneProps() { return sceneProps; }
+
+    // void rotate(float nAngle) { angle = nAngle; }
+    //  void setOrigin(Vec2 nOrigin) { origin = nOrigin; }
+    void setColor(Color nColor) { color = nColor; }
+
+    virtual void setFlippedX(bool flip)
+    {
+      this->setDirty();
+      flippedX = flip;
+    };
+
+    void setOpacity(float opacity)
+    {
+      this->setDirty();
+      color = {color.r, color.g, color.b, (unsigned char)opacity};
+    }
+  };
 } // namespace dudis
