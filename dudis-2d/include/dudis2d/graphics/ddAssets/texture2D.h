34c61644773aeba78da18a0ac719fb8768f67b93
Math3uso <matheusfred955@gmail.com>
2026-02-18 20:04:31 -0300

    graphics: track DDTextures cache

--- dudis-2d/include/dudis2d/graphics/ddAssets/texture2D.h
+++ dudis-2d/include/dudis2d/graphics/ddAssets/texture2D.h
@@ -5,29 +5,29 @@
 #include <string>
 #include <unordered_map>
 
-namespace dudis {
+namespace dudis
+{
 
-class Sprite;
+  class Sprite;
 
-namespace res {
+  namespace res
+  {
 
-class Texture2D;
+    class Texture2D;
 
-class Texture2D {
-private:
-  static std::unordered_map<std::string, rl::RlTexture> _rlTextures;
-  static DDTexture _create(const char *path, DDTexture &ddTex, uint32_t *id);
-  friend DDTexture;
+    class Texture2D
+    {
+    private:
+      static std::unordered_map<std::string, rl::RlTexture> _rlTextures;
+      static std::unordered_map<std::string, DDTexture> _ddTextures;
+      static DDTexture _create(const char *path, DDTexture &ddTex, uint32_t *id);
+      friend DDTexture;
 
-public:
-  // static DDTexture create(const char *path,
-  //                         TextureLoadMode mode = TextureLoadMode::Uncached);
+    public:
+      static DDTexture create(const char *path);
 
-  static DDTexture create(const char *path);
-
-  // static DDTexture create(const TextureInfo &info);
-  static void unload(const DDTexture &tex);
-  static DDTexture _rlTex2DFromDDTex(const rl::RlTexture &rltex);
-};
-} // namespace res
+      static void unload(const DDTexture &tex);
+      static DDTexture _rlTex2DFromDDTex(const rl::RlTexture &rltex);
+    };
+  } // namespace res
 } // namespace dudis
