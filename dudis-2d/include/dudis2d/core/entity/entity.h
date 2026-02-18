d8bee2456c48afbbe1fcbffd546f2028f94021af
Math3uso <matheusfred955@gmail.com>
2026-02-18 20:04:31 -0300

    core: extend Entity for render commands and SizeF

--- dudis-2d/include/dudis2d/core/entity/entity.h
+++ dudis-2d/include/dudis2d/core/entity/entity.h
@@ -10,194 +10,227 @@
 #include <typeindex>
 #include <unordered_map>
 
-namespace dudis {
+namespace dudis
+{
 
-class Motion;
-class Component;
-// class PhysicsComponent;
-
-/**
- * @brief Tipo de transformação de uma entidade em relação ao seu pai.
- *
- * Define como a transformação local de uma entidade se combina com a
- * transformação do seu elemento pai.
- */
-enum class TransformType {
-  /**
-   * @brief Transformação relativa ao pai (padrão).
-   *
-   * O filho segue todas as transformações do pai. Ou seja,
-   * posição, rotação e escala do pai influenciam diretamente no filho.
-   */
-  Relative,
+  class Motion;
+  class Component;
+  class RenderQueue;
+  class Renderable;
+  // class PhysicsComponent;
 
   /**
-   * @brief Transformação absoluta.
+   * @brief Tipo de transformação de uma entidade em relação ao seu pai.
    *
-   * A entidade ignora as transformações do pai e usa apenas sua
-   * posição, rotação e escala locais. Somente esta entidade é afetada.
+   * Define como a transformação local de uma entidade se combina com a
+   * transformação do seu elemento pai.
    */
-  Absolute,
+  enum class TransformType
+  {
+    /**
+     * @brief Transformação relativa ao pai (padrão).
+     *
+     * O filho segue todas as transformações do pai. Ou seja,
+     * posição, rotação e escala do pai influenciam diretamente no filho.
+     */
+    Relative,
 
-  /**
-   * @brief Ignora transformações do pai para todos os filhos.
-   *
-   * Se um pai tiver este tipo, todos os seus filhos passam a ser
-   * tratados como Absolute, ou seja, não recebem nenhuma
-   * influência da transformação do pai.
-   */
-  DisableParentTransform
-};
+    /**
+     * @brief Transformação absoluta.
+     *
+     * A entidade ignora as transformações do pai e usa apenas sua
+     * posição, rotação e escala locais. Somente esta entidade é afetada.
+     */
+    Absolute,
 
-struct DDBufferPtr {
-  void *ptr;
-  void (*deleter)(void *);
-  size_t size;
-};
+    /**
+     * @brief Ignora transformações do pai para todos os filhos.
+     *
+     * Se um pai tiver este tipo, todos os seus filhos passam a ser
+     * tratados como Absolute, ou seja, não recebem nenhuma
+     * influência da transformação do pai.
+     */
+    DisableParentTransform
+  };
+
+  struct DDBufferPtr
+  {
+    void *ptr;
+    void (*deleter)(void *);
+    size_t size;
+  };
 
 #define DD_LOGS_ACTIVE 0
 #define DD_NO_LOGS 1
 
-class Entity {
+  class Entity
+  {
 
-private:
-  bool _localDirty = false;
-  bool _globalDirty = true;
-  bool _orderChildren = false;
-  glm::mat4 _localMatrix = glm::mat4(1.0f);
-  glm::mat4 _globalMatrix = glm::mat4(1.0f);
-  std::unordered_map<std::type_index, std::shared_ptr<Component>> _components;
-  bool _physicsComponent = false;
-  TransformType _transformType = TransformType::Relative;
-  int _zOrder = 0;
-  std::vector<DDBufferPtr> _owned;
+  private:
+    bool _localDirty = false;
+    bool _globalDirty = true;
+    bool _orderChildren = false;
+    glm::mat4 _localMatrix = glm::mat4(1.0f);
+    glm::mat4 _globalMatrix = glm::mat4(1.0f);
+    std::unordered_map<std::type_index, std::shared_ptr<Component>> _components;
+    bool _physicsComponent = false;
+    TransformType _transformType = TransformType::Relative;
+    int _zOrder = 0;
+    std::vector<DDBufferPtr> _owned;
 
-  template <typename T> static void _destroy(void *p) {
-    delete static_cast<T *>(p);
-  }
+    template <typename T>
+    static void _destroy(void *p)
+    {
+      delete static_cast<T *>(p);
+    }
 
 #ifdef DD_DEBUG
-  static constexpr bool _DD_LOGS = DD_LOGS_ACTIVE;
+    static constexpr bool _DD_LOGS = DD_LOGS_ACTIVE;
 #else
-  static constexpr bool _DD_LOGS = DD_NO_LOGS;
+    static constexpr bool _DD_LOGS = DD_NO_LOGS;
 #endif
 
-protected:
-  dudis::SizeI size = {0, 0};
-  dudis::Vec2 pos = {0, 0};
-  Vec2 origin = {0, 0};
-  Vec2 scale = {1, 1};
-  float angle = 0.0f; // in radians
-  std::vector<std::shared_ptr<Motion>> actions;
-  std::shared_ptr<Motion> _action;
-  std::vector<std::shared_ptr<Entity>> _children;
-  Entity *_parent = nullptr;
-  bool _isUpdated = false;
-  void _sortChildrenByIndex();
+  protected:
+    dudis::SizeF size = {0.f, 0.f};
+    dudis::Vec2 pos = {0, 0};
+    Vec2 origin = {0, 0};
+    Vec2 scale = {1, 1};
+    float angle = 0.0f; // in radians
+    std::vector<std::shared_ptr<Motion>> actions;
+    std::shared_ptr<Motion> _action;
+    std::vector<std::shared_ptr<Entity>> _children;
+    Entity *_parent = nullptr;
+    bool _isUpdated = false;
+    void _sortChildrenByIndex();
 
-public:
-  Entity() = default;
-  float getAngle() const { return angle; }
-  Vec2 getPos() const { return pos; }
-  SizeI getSize() const { return size; }
-  Vec2 getOrigin() const { return origin; }
-  // void alloc(T* buff){}
+  public:
+    char *labelT;
+    Entity() = default;
+    float getAngle() const { return angle; }
+    Vec2 getPos() const { return pos; }
+    SizeF getSize() const { return size; }
+    Vec2 getOrigin() const { return origin; }
+    // void alloc(T* buff){}
 
-  const Vec2 getGlobalPos();
-  float getGlobalRotation();
-  const Vec2 getGlocalScale();
-  Size getGlobalSize();
+    const Vec2 getGlobalPos();
+    float getGlobalRotation();
+    const Vec2 getGlocalScale();
+    Size getGlobalSize();
 
-  void setTransformType(TransformType type) { _transformType = type; }
+    void setTransformType(TransformType type) { _transformType = type; }
 
-  virtual void setPos(const dudis::Vec2 &nPos);
-  void setPos(float x, float y);
-  void setZOrder(int zIndex);
+    virtual void setPos(const dudis::Vec2 &nPos);
+    void setPos(float x, float y);
+    void setZOrder(int zIndex);
 
-  virtual void setSize(dudis::SizeI nSize);
-  virtual void translate(const dudis::Vec2 &nPos);
+    virtual void setSize(dudis::SizeF nSize);
+    virtual void translate(const dudis::Vec2 &nPos);
 
-  virtual void rotate(float nAngle);
-  virtual void start() {};
-  virtual void init() {};
-  virtual void update() {};
-  virtual void render();
-  virtual void updateChildren();
+    virtual void rotate(float nAngle);
+    virtual void start() {};
+    virtual void init() {};
+    virtual void update() {};
+    virtual void render();
+    virtual void updateChildren();
+    virtual void buildRenderCommands(RenderQueue *queue) {};
 
-  void defaultUpdate();
-  void runMotions();
-  void setDirty();
+    bool intersectsWith(const std::shared_ptr<Entity> &other);
 
-  const glm::mat4 &getLocalMatrix();
-  const glm::mat4 &getGlobalMatrix();
+    void defaultUpdate();
+    void runMotions();
+    void setDirty();
 
-  void setParent(Entity *parent) { _parent = parent; };
-  void setOrigin(Vec2 nOrigin) { origin = nOrigin; }
-  void setMotion(std::shared_ptr<Motion> action) {
-    // this->actions.push_back(action);
-    _action = action;
-  };
+    Rect getBoundingBox();
 
-  void addChild(std::shared_ptr<Entity> entity);
-  void addChild(std::shared_ptr<Entity> entity, const int zOrder);
+    const glm::mat4 &getLocalMatrix();
+    const glm::mat4 &getGlobalMatrix();
 
-  const std::vector<std::shared_ptr<Entity>> &getChildren() {
-    //_sortChildrenByIndex();
-    return _children;
-  };
+    void setParent(Entity *parent) { _parent = parent; };
+    void setOrigin(Vec2 nOrigin) { origin = nOrigin; }
+    void setMotion(std::shared_ptr<Motion> action)
+    {
+      // this->actions.push_back(action);
+      _action = action;
+    };
 
-  template <typename T, typename... Args>
-  std::shared_ptr<T> addComponent(Args &&...args) {
-    auto comp = std::make_shared<T>(std::forward<Args>(args)...);
-    _components[typeid(T)] = comp;
+    void addChild(std::shared_ptr<Entity> entity);
+    void addChild(std::shared_ptr<Entity> entity, const int zOrder);
 
-    if (auto phys = std::dynamic_pointer_cast<PhysicsComponent>(comp)) {
-      _physicsComponent = true;
-      puts("compoenent fisico enviado");
-    }
+    const std::vector<std::shared_ptr<Entity>> &getChildren()
+    {
+      //_sortChildrenByIndex();
+      return _children;
+    };
 
-    return comp;
-  }
+    template <typename T, typename... Args>
+    std::shared_ptr<T> addComponent(Args &&...args)
+    {
+      auto comp = std::make_shared<T>(std::forward<Args>(args)...);
+      _components[typeid(T)] = comp;
 
-  // versão para usar instância já criada
-  template <typename T>
-  std::shared_ptr<T> addComponent(std::shared_ptr<T> &comp) {
-    _components[typeid(T)] = comp;
-
-    if (auto phys = std::dynamic_pointer_cast<PhysicsComponent>(comp)) {
-      _physicsComponent = true;
-      if (_DD_LOGS) {
-        puts("component fisico enviado [INSTANCIA]");
+      if (auto phys = std::dynamic_pointer_cast<PhysicsComponent>(comp))
+      {
+        _physicsComponent = true;
+        puts("compoenent fisico enviado");
       }
+
+      return comp;
     }
 
-    return comp;
-  }
+    // versão para usar instância já criada
+    template <typename T>
+    std::shared_ptr<T> addComponent(std::shared_ptr<T> &comp)
+    {
+      _components[typeid(T)] = comp;
 
-  template <typename T> std::shared_ptr<T> getComponent() {
-    auto it = _components.find(typeid(T));
-    if (it != _components.end())
-      return std::static_pointer_cast<T>(it->second);
-    return nullptr;
-  }
+      if (auto phys = std::dynamic_pointer_cast<PhysicsComponent>(comp))
+      {
+        _physicsComponent = true;
+        if (_DD_LOGS)
+        {
+          puts("component fisico enviado [INSTANCIA]");
+        }
+      }
 
-  template <typename T> void removeComponent() { _components.erase(typeid(T)); }
-
-  template <typename T, typename... Args> T *New(Args &&...args) {
-    T *data = new T(std::forward<Args>(args)...);
-
-    if constexpr (_DD_LOGS) {
-      _owned.push_back(DDBufferPtr{data, &_destroy<T>, sizeof(*data)});
-    } else {
-      _owned.push_back(DDBufferPtr{data, &_destroy<T>, 0});
+      return comp;
     }
-    return data;
+
+    template <typename T>
+    std::shared_ptr<T> getComponent()
+    {
+      auto it = _components.find(typeid(T));
+      if (it != _components.end())
+        return std::static_pointer_cast<T>(it->second);
+      return nullptr;
+    }
+
+    template <typename T>
+    void removeComponent() { _components.erase(typeid(T)); }
+
+    template <typename T, typename... Args>
+    T *New(Args &&...args)
+    {
+      T *data = new T(std::forward<Args>(args)...);
+
+      if constexpr (_DD_LOGS)
+      {
+        _owned.push_back(DDBufferPtr{data, &_destroy<T>, sizeof(*data)});
+      }
+      else
+      {
+        _owned.push_back(DDBufferPtr{data, &_destroy<T>, 0});
+      }
+      return data;
+    };
+
+    template <typename T>
+    static std::shared_ptr<T> create()
+    {
+      return std::make_shared<T>();
+    }
+
+    virtual ~Entity();
+
+    friend Renderable;
   };
-
-  template <typename T> static std::shared_ptr<T> create() {
-    return std::make_shared<T>();
-  }
-
-  virtual ~Entity();
-};
 } // namespace dudis
