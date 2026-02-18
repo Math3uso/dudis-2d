9906678e3f16fbd12525cb8272069021e9fe80e5
Math3uso <matheusfred955@gmail.com>
2026-02-18 20:04:31 -0300

    docs(input): document Input::update

--- dudis-2d/include/dudis2d/core/input/input.h
+++ dudis-2d/include/dudis2d/core/input/input.h
@@ -5,6 +5,11 @@ namespace dudis
     class Input
     {
     public:
+        /**
+         * @brief Atualiza o estado de teclado e mouse.
+         *
+         * Deve ser chamado uma vez por frame no loop da janela.
+         */
         static void update();
     };
 }
