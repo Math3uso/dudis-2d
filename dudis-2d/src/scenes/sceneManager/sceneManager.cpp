fe30a73b084219625f278f1a5795a17e6da75d08
Math3uso <matheusfred955@gmail.com>
2026-02-18 20:04:32 -0300

    scene: simplify SceneManager applyChange

--- dudis-2d/src/scenes/sceneManager/sceneManager.cpp
+++ dudis-2d/src/scenes/sceneManager/sceneManager.cpp
@@ -5,19 +5,25 @@
 
 Scene *SceneManager::getScene() { return current.get(); }
 
-bool SceneManager::setScene(dudis::Scope<Scene> nScene) {
+bool SceneManager::setScene(dudis::Scope<Scene> nScene)
+{
   _replaceMode = true;
   nextScene = std::move(nScene);
   return true;
 }
 
-void SceneManager::popScene() {
+void SceneManager::popScene()
+{
 
-  if (!scenes.empty()) {
+  if (!scenes.empty())
+  {
     _pendingPop = true;
-    if (_index > 0) {
+    if (_index > 0)
+    {
       _index -= 1;
-    } else {
+    }
+    else
+    {
       _index = 0;
     }
   }
@@ -27,13 +33,15 @@ void SceneManager::popScene() {
             << this->getCurrentScene()->label << "\n";
 }
 
-void SceneManager::pushScene(dudis::Scope<Scene> nScene) {
+void SceneManager::pushScene(dudis::Scope<Scene> nScene)
+{
 
   dudis::Log::Info("================ Push Scene ================");
 
   dudis::Log::Info("preparando Scene para add na lista");
 
-  if (nScene) {
+  if (nScene)
+  {
     scenes.push_back(std::move(nScene));
     _index = scenes.size() - 1;
     _pendingPush = true;
@@ -44,20 +52,26 @@ void SceneManager::pushScene(dudis::Scope<Scene> nScene) {
 
 void SceneManager::releaseCurrentScene() {}
 
-void SceneManager::applyChangeScene() {
+void SceneManager::applyChangeScene()
+{
   if (scenes.empty())
     return;
 
-  if (_pendingPush) {
+  if (_pendingPush)
+  {
     scenes.back()->start();
     scenes.back()->init();
     _pendingPush = false;
-  } else if (_pendingPop) {
+  }
+  else if (_pendingPop)
+  {
     scenes.pop_back();
     scenes.back()->init();
     // scenes.back()->start();
     _pendingPop = false;
-  } else if (_replaceMode) {
+  }
+  else if (_replaceMode)
+  {
     scenes[_index] = std::move(nextScene);
     scenes[_index]->start();
     scenes[_index]->init();
@@ -73,17 +87,20 @@ size_t SceneManager::getTotalScenes() { return scenes.size(); }
 
 Scene *SceneManager::getCurrentScene() { return scenes[_index].get(); };
 
-void SceneManager::dispose() {
+void SceneManager::dispose()
+{
 
   dudis::Log::Info("================ dispose scenes ================\n");
 
   dudis::Log::Alert("scenes sendo deletadas...");
 
-  if (current && current.get()->getFrameBuffer().id > 0) {
+  if (current)
+  {
     releaseCurrentScene();
   }
 
-  if (!scenes.empty()) {
+  if (!scenes.empty())
+  {
 
     scenes.clear();
 
