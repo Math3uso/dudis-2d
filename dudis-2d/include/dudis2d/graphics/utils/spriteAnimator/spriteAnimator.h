4599aef3f9eaa919b0fb4edaa8664345207e25d0
Math3uso <matheusfred955@gmail.com>
2026-02-18 20:04:31 -0300

    graphics: update SpriteAnimator types

--- dudis-2d/include/dudis2d/graphics/utils/spriteAnimator/spriteAnimator.h
+++ dudis-2d/include/dudis2d/graphics/utils/spriteAnimator/spriteAnimator.h
@@ -2,41 +2,40 @@
 
 #include "dudis2d/graphics/sprites/sprite/sprite.h"
 
-namespace dudis {
-
-class SpriteAnimator : public Renderable
+namespace dudis
 {
-protected:
-  std::shared_ptr<Sprite> sprite;
-  int totalFrames = 0;
-  int currentFrame = 0;
-  float frameTime = 0.1f; // seconds
-  float timer = 0.0f;
-  SizeI frameSize;
 
-  bool loop = true;
+  class SpriteAnimator : public Renderable
+  {
+  protected:
+    std::shared_ptr<Sprite> sprite;
+    int totalFrames = 0;
+    int currentFrame = 0;
+    float frameTime = 0.1f; // seconds
+    float timer = 0.0f;
+    SizeF frameSize;
 
-public:
-  SpriteAnimator(std::shared_ptr<Sprite>);
+    bool loop = true;
 
-  void update() override;
-  void render() override;
+  public:
+    SpriteAnimator(std::shared_ptr<Sprite>);
 
-  void reset();
-  void setLoop(bool enable);
-  void setAnimation(int total, SizeI size, float fps);
+    void update() override;
+    void render() override;
 
-  void setSize(SizeI size) override;
-  void setPos(Vec2 pos) override;
-  void setFlippedX(bool flip) override;
+    void reset();
+    void setLoop(bool enable);
+    void setAnimation(int total, SizeF size, float fps);
 
-  const std::shared_ptr<Sprite> &getSpriteRef() const {
-    return sprite;
+    void setSize(SizeF size) override;
+    void setPos(const Vec2 &pos) override;
+    void setFlippedX(bool flip) override;
+
+    const std::shared_ptr<Sprite> &getSpriteRef() const { return sprite; };
+
+    std::function<void()> onAnimationEnd = nullptr;
+
+    static std::shared_ptr<SpriteAnimator>
+    create(std::shared_ptr<Sprite> spriteRef);
   };
-
-  std::function<void()> onAnimationEnd = nullptr;
-
-  static std::shared_ptr<SpriteAnimator>
-  create(std::shared_ptr<Sprite> spriteRef);
-};
 } // namespace dudis
