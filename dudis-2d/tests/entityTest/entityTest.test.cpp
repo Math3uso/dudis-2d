bc75fcf5ad4e9071157a5cafe628cf5ee3d4494f
Math3uso <matheusfred955@gmail.com>
2026-03-03 22:47:08 -0300

    test: create EntityTest

---
+++ dudis-2d/tests/entityTest/entityTest.test.cpp
@@ -0,0 +1,54 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include "dudis2d/dudis2d.h"
+#include "dudis2d/debug.h"
+
+using namespace dudis;
+
+TEST_CASE("EntityTest test")
+{
+    SECTION("It should be able to create a entity")
+    {
+
+        auto entity = Entity::create("entity");
+
+        REQUIRE(entity->getTag() == "entity");
+    }
+
+    SECTION("It should be able to add a child in entity")
+    {
+
+        auto entity = Entity::create("entity");
+        auto entity2 = Entity::create("entity2");
+
+        entity->addChild(entity2);
+
+        REQUIRE(entity->getChildren().size() == 1);
+    }
+
+    SECTION("It should be able to remove a child in entity")
+    {
+
+        auto entity = Entity::create("entity");
+        auto entity2 = Entity::create("entity2");
+
+        entity->addChild(entity2);
+
+        entity->removeChild();
+
+        REQUIRE(entity->getChildren().size() == 0);
+    }
+
+    SECTION("It should be able to remove a child in entity by tag")
+    {
+
+        auto entity = Entity::create("entity");
+        auto entity2 = Entity::create("entity2");
+
+        entity->addChild(entity2);
+
+        entity->removeChild("entity2");
+
+        REQUIRE(entity->getChildren().size() == 0);
+    }
+}
