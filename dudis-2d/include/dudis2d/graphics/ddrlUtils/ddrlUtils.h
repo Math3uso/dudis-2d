43d99fdd1b391c83e9acb8c4c44a6da5a936eb78
Math3uso <matheusfred955@gmail.com>
2026-03-15 15:20:30 -0300

    feat: add RL filter conversions to dudis-2d

--- dudis-2d/include/dudis2d/graphics/ddrlUtils/ddrlUtils.h
+++ dudis-2d/include/dudis2d/graphics/ddrlUtils/ddrlUtils.h
@@ -1,14 +1,18 @@
 #pragma once
 
 #include "dudis2d/graphics/ddAssets/ddTextureTypeUtils.h"
+#include "dudis2d/graphics/ddAssets/ddTextureFilter.h"
 #include "raylib.h"
 
 namespace dudis
 {
+    // Usar o prefixo RL para nas funções desse namespace
     namespace ddrlUtils
     {
         res::TextureFormat mapRaylibFormat(PixelFormat fmt);
 
         PixelFormat mapToRaylibFormat(res::TextureFormat fmt);
+
+        unsigned int mapToRLFilter(res::DDTextureFilter filter);
     } // namespace ddrlUtils
 } // namespace dudis
--- dudis-2d/src/graphics/ddrlUtils/ddrlUtils.cpp
+++ dudis-2d/src/graphics/ddrlUtils/ddrlUtils.cpp
@@ -1,37 +1,56 @@
 #include "dudis2d/graphics/ddrlUtils/ddrlUtils.h"
 
-namespace dudis {
-namespace ddrlUtils {
+namespace dudis
+{
+  namespace ddrlUtils
+  {
 
-res::TextureFormat mapRaylibFormat(PixelFormat fmt) {
-  switch (fmt) {
-  case PIXELFORMAT_UNCOMPRESSED_R8G8B8A8:
-    return res::TextureFormat::RGBA8;
-  case PIXELFORMAT_UNCOMPRESSED_R8G8B8:
-    return res::TextureFormat::RGB8;
-  case PIXELFORMAT_UNCOMPRESSED_GRAYSCALE:
-    return res::TextureFormat::R8;
-  case PIXELFORMAT_COMPRESSED_DXT1_RGB:
-    return res::TextureFormat::DXT1_RGB;
-  default:
-    return res::TextureFormat::RGBA8;
-  }
-}
+    res::TextureFormat mapRaylibFormat(PixelFormat fmt)
+    {
+      switch (fmt)
+      {
+      case PIXELFORMAT_UNCOMPRESSED_R8G8B8A8:
+        return res::TextureFormat::RGBA8;
+      case PIXELFORMAT_UNCOMPRESSED_R8G8B8:
+        return res::TextureFormat::RGB8;
+      case PIXELFORMAT_UNCOMPRESSED_GRAYSCALE:
+        return res::TextureFormat::R8;
+      case PIXELFORMAT_COMPRESSED_DXT1_RGB:
+        return res::TextureFormat::DXT1_RGB;
+      default:
+        return res::TextureFormat::RGBA8;
+      }
+    }
 
-PixelFormat mapToRaylibFormat(res::TextureFormat fmt) {
-  switch (fmt) {
-  case res::TextureFormat::RGBA8:
-    return PIXELFORMAT_UNCOMPRESSED_R8G8B8A8;
-  case res::TextureFormat::RGB8:
-    return PIXELFORMAT_UNCOMPRESSED_R8G8B8;
-  case res::TextureFormat::R8:
-    return PIXELFORMAT_UNCOMPRESSED_GRAYSCALE;
-  case res::TextureFormat::DXT1_RGB:
-    return PIXELFORMAT_COMPRESSED_DXT1_RGB;
-  default:
-    return PIXELFORMAT_UNCOMPRESSED_R8G8B8A8;
-  }
-}
+    PixelFormat mapToRaylibFormat(res::TextureFormat fmt)
+    {
+      switch (fmt)
+      {
+      case res::TextureFormat::RGBA8:
+        return PIXELFORMAT_UNCOMPRESSED_R8G8B8A8;
+      case res::TextureFormat::RGB8:
+        return PIXELFORMAT_UNCOMPRESSED_R8G8B8;
+      case res::TextureFormat::R8:
+        return PIXELFORMAT_UNCOMPRESSED_GRAYSCALE;
+      case res::TextureFormat::DXT1_RGB:
+        return PIXELFORMAT_COMPRESSED_DXT1_RGB;
+      default:
+        return PIXELFORMAT_UNCOMPRESSED_R8G8B8A8;
+      }
+    }
 
-} // namespace ddrlUtils
+    unsigned int mapToRLFilter(res::DDTextureFilter filter)
+    {
+      switch (filter)
+      {
+      case res::TextureFilter::Smooth:
+        return TEXTURE_FILTER_BILINEAR;
+      case res::TextureFilter::Nearest:
+        return TEXTURE_FILTER_POINT;
+      default:
+        return TEXTURE_FILTER_POINT;
+      }
+    }
+
+  } // namespace ddrlUtils
 } // namespace dudis
