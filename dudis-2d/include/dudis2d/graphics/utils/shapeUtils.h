dd41c03e56f8e16f63e58ee646c30ee46d2e2b21
Math3uso <matheusfred955@gmail.com>
2026-03-27 12:13:34 -0300

    fix: remove method FullTopLeft in shapeUtils.cpp

--- dudis-2d/include/dudis2d/graphics/utils/shapeUtils.h
+++ dudis-2d/include/dudis2d/graphics/utils/shapeUtils.h
@@ -4,13 +4,15 @@
 
 // class Renderable;
 
-namespace dudis {
-class ShapeUtils {
-public:
-  static dudis::Vec2 Center(std::weak_ptr<Renderable> shape);
-  static dudis::Vec2 FullTopLeft(std::weak_ptr<Renderable> shape);
-  // static dudis::Vec2 FullTopRight(std::weak_ptr<Renderable> shape) {};
-  // static dudis::Vec2 FullBottomRight(std::weak_ptr<Renderable> shape) {};
-  // static dudis::Vec2 FullBottomLeft(std::weak_ptr<Renderable> shape) {};
-};
+namespace dudis
+{
+  class ShapeUtils
+  {
+  public:
+    static dudis::Vec2 Center(std::weak_ptr<Renderable> shape);
+    static dudis::Vec2 FullTopLeft(std::weak_ptr<Renderable> shape);
+    // static dudis::Vec2 FullTopRight(std::weak_ptr<Renderable> shape) {};
+    // static dudis::Vec2 FullBottomRight(std::weak_ptr<Renderable> shape) {};
+    // static dudis::Vec2 FullBottomLeft(std::weak_ptr<Renderable> shape) {};
+  };
 } // namespace dudis
