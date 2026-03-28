8c3f3c1a8baab8b34230fe403178b4f4fa2fab30
Math3uso <matheusfred955@gmail.com>
2026-03-28 20:06:36 -0300

    refactor(graphics): clean up legacy layer and framebuffer stubs and normalize queue header

--- dudis-2d/include/dudis2d/graphics/frameQueue/frameQueue.h
+++ dudis-2d/include/dudis2d/graphics/frameQueue/frameQueue.h
@@ -2,28 +2,28 @@
 
 #include <atomic>
 
-namespace dudis {
+namespace dudis
+{
 
 #define DD_INDEX_READ 1
 #define DD_INDEX_WRITE 1
 
-class RenderQueue;
+  class RenderQueue;
 
-class FrameQueue {
-private:
-  // std::atomic<uint32_t> _writeIndex = 0;
-  // std::atomic<uint32_t> _readIndex = 1;
-  std::atomic<uint32_t> _index = 0;
-  uint32_t _writeIndex = 1;
-  RenderQueue *_queues[2];
+  class FrameQueue
+  {
+  private:
+    std::atomic<uint32_t> _index = 0;
+    uint32_t _writeIndex = 1;
+    RenderQueue *_queues[2];
 
-public:
-  FrameQueue() = default;
-  ~FrameQueue();
-  void init();
-  RenderQueue *beginWrite();
-  void endWrite();
+  public:
+    FrameQueue() = default;
+    ~FrameQueue();
+    void init();
+    RenderQueue *beginWrite();
+    void endWrite();
 
-  const RenderQueue *beginRead() const;
-};
+    const RenderQueue *beginRead() const;
+  };
 }; // namespace dudis
