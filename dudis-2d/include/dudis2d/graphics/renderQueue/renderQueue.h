b0e2a88b12c027838772ff9e226b2e44600b44bb
Math3uso <matheusfred955@gmail.com>
2026-03-29 23:46:21 -0300

    feat: add const getCommands

--- dudis-2d/include/dudis2d/graphics/renderQueue/renderQueue.h
+++ dudis-2d/include/dudis2d/graphics/renderQueue/renderQueue.h
@@ -3,24 +3,28 @@
 #include "dudis2d/graphics/drawCommand/drawCommand.h"
 #include <vector>
 
-namespace dudis {
-class RenderQueue {
-private:
-  std::vector<DrawCommand> _renderList;
-  uint32_t _size = 500;
-  uint32_t _current = 0;
-  void _checkRelocation();
+namespace dudis
+{
+  class RenderQueue
+  {
+  private:
+    std::vector<DrawCommand> _renderList;
+    uint32_t _size = 500;
+    uint32_t _current = 0;
+    void _checkRelocation();
 
-public:
-  RenderQueue() { _renderList.reserve(_size); };
+  public:
+    RenderQueue() { _renderList.reserve(_size); };
 
-  void addCommand(const DrawCommand &cmd);
-  void addCommand(const std::vector<DrawCommand> &&renderList);
-  const std::vector<DrawCommand> &getCommands() const { return _renderList; }
-  void clear();
+    void addCommand(const DrawCommand &cmd);
+    void addCommand(const std::vector<DrawCommand> &&renderList);
+    std::vector<DrawCommand> &getCommands() { return _renderList; }
+    const std::vector<DrawCommand> &getCommands() const { return _renderList; }
+    void clear();
 
-  static std::unique_ptr<RenderQueue> create() {
-    return std::make_unique<RenderQueue>();
-  }
-};
+    static std::unique_ptr<RenderQueue> create()
+    {
+      return std::make_unique<RenderQueue>();
+    }
+  };
 } // namespace dudis
