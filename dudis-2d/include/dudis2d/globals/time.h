1df927dcf95d1d9b1747f6642cd011a6eeaeba09
Math3uso <matheusfred955@gmail.com>
2026-04-02 19:52:28 -0300

    feat: add time class

---
+++ dudis-2d/include/dudis2d/globals/time.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace dudis
+{
+    class Time
+    {
+    public:
+        static float deltaTime;
+        static float fixedDeltaTime;
+        static float timeScele;
+    };
+}
---
+++ dudis-2d/src/globals/time.cpp
@@ -0,0 +1,7 @@
+#include "dudis2d/globals/time.h"
+
+using namespace dudis;
+
+float Time::deltaTime = 0.f;
+float Time::fixedDeltaTime = 1.0f / 60.f;
+float Time::timeScele = 1.0f;
