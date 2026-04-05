0ec0ac474f08d64219e74a3fdd9fdf2059f06758
Math3uso <matheusfred955@gmail.com>
2026-04-05 13:54:03 -0300

    feat: converting TextureFormat to PixelFormat(rl)

---
+++ dudis-2d/include/dudis2d/core/ddrlUtils/toRLTextureFormat.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "dudis2d/graphics/ddAssets/ddTextureTypeUtils.h"
+
+namespace dudis
+{
+    namespace ddrlUtils
+    {
+        unsigned int toRLTextureFormat(res::TextureFormat ddTexFormat);
+    }
+}
---
+++ dudis-2d/src/core/ddrlUtils/toRLTextureFormat.cpp
@@ -0,0 +1,25 @@
+#include "dudis2d/core/ddrlUtils/toRLTextureFormat.h"
+#include "raylib.h"
+
+namespace dudis
+{
+    namespace ddrlUtils
+    {
+        unsigned int toRLTextureFormat(res::TextureFormat ddTexFormat)
+        {
+            switch (ddTexFormat)
+            {
+            case res::TextureFormat::RGBA8:
+                return PIXELFORMAT_UNCOMPRESSED_R8G8B8A8;
+            case res::TextureFormat::RGB8:
+                return PIXELFORMAT_UNCOMPRESSED_R8G8B8;
+            case res::TextureFormat::R8:
+                return PIXELFORMAT_UNCOMPRESSED_GRAYSCALE;
+            case res::TextureFormat::DXT1_RGB:
+                return PIXELFORMAT_COMPRESSED_DXT1_RGB;
+            default:
+                return PIXELFORMAT_UNCOMPRESSED_R8G8B8A8;
+            }
+        }
+    }
+}
