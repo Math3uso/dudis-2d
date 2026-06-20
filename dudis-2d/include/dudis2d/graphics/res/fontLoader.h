4df13c2618864e66f6696d3b0846888f9dcfcee6
Math3uso <matheusfred955@gmail.com>
2026-06-20 00:00:04 -0300

    feat: creat createTTFBuffer and createFontWithTTFBuffer

--- dudis-2d/include/dudis2d/graphics/res/fontLoader.h
+++ dudis-2d/include/dudis2d/graphics/res/fontLoader.h
@@ -2,6 +2,8 @@
 
 #include <vector>
 #include <array>
+#include <memory>
+// #include <optional>
 #include "stb_truetype.h"
 
 namespace dudis
@@ -12,7 +14,17 @@ namespace dudis
         {
             std::vector<unsigned char> bitmaps;
             std::array<stbtt_bakedchar, 96> cdata;
+            int bitmapW;
+            int bitmapH;
+            float pixelH;
         };
+
+        // constexpr FontData DD_INVALIDE_FONTE_DATA = {};
+        constexpr std::vector<unsigned char> DD_INVALIDE_TTF_BUFFER;
+
+        std::vector<unsigned char> createTTFBuffer(const char *path);
+        FontData createFontWithTTFBuffer(std::vector<unsigned char> &buffer, float size = 32.f);
+
         FontData loadTTF(const char *path);
     };
 }
--- dudis-2d/src/graphics/res/fontLoader.cpp
+++ dudis-2d/src/graphics/res/fontLoader.cpp
@@ -13,6 +13,9 @@ FontLoader::FontData FontLoader::loadTTF(const char *path)
 
     FontData data;
 
+    int fontAtlasW = 512;
+    int fontAtlasH = 512;
+
     // abre indo pro final
     std::ifstream f(path, std::ios::binary | std::ios::ate);
 
@@ -27,22 +30,96 @@ FontLoader::FontData FontLoader::loadTTF(const char *path)
 
     stbtt_fontinfo font;
 
-    stbtt_InitFont(&font, ttfBuffer.data(), stbtt_GetFontOffsetForIndex(ttfBuffer.data(), 0));
+    int offset = stbtt_GetFontOffsetForIndex(ttfBuffer.data(), 0);
+    if (offset < 0 || !stbtt_InitFont(&font, ttfBuffer.data(), offset))
+    {
+        return data; // Arquivo corrompido ou formato incompatível
+    }
+
+    // stbtt_InitFont(&font, ttfBuffer.data(), stbtt_GetFontOffsetForIndex(ttfBuffer.data(), 0));
 
     // Isso é um array de pixels (imagem) em memória.
     // cada pixel indo de 0 - 255 (grayScale)
-    unsigned char bitmap[512 * 512];
+    std::vector<unsigned char> bitmap(fontAtlasW * fontAtlasH, 0);
+    data.bitmaps.resize(96 - 32);
+    // unsigned char bitmap[fontAtlasW * fontAtlasH];
 
     stbtt_BakeFontBitmap(
-        ttfBuffer.data(), 0,
+        ttfBuffer.data(), offset,
         32.f,
-        bitmap, 512, 512,
+        bitmap.data(), fontAtlasW, fontAtlasH,
         32, 96,
         data.cdata.data()
         //
     );
 
-    data.bitmaps.assign(bitmap, bitmap + sizeof(bitmap));
+    // data.bitmaps.assign(bitmap, bitmap + sizeof(bitmap));
+    data.bitmaps = std::move(bitmap);
+    data.bitmapW = fontAtlasW;
+    data.bitmapH = fontAtlasH;
+    data.pixelH = 32;
+
+    return data;
+}
+
+// std::vector<unsigned char> createTTFBuffer(const char *path)
+// {
+// }
+
+vector<unsigned char> FontLoader::createTTFBuffer(const char *path)
+{
+
+    vector<unsigned char> ttfBuffer;
+    int fontAtlasW = 512;
+    int fontAtlasH = 512;
+
+    std::ifstream f(path, std::ios::binary | std::ios::ate);
+
+    if (!f.is_open())
+    {
+        puts("[ERRO] invalide path");
+        return FontLoader::DD_INVALIDE_TTF_BUFFER;
+    }
+    std::streamsize size = f.tellg();
+    f.seekg(0, std::ios::beg);
+    ttfBuffer.resize(size);
+    f.read(reinterpret_cast<char *>(ttfBuffer.data()), size);
+    f.close();
+
+    return ttfBuffer;
+};
+
+FontLoader::FontData FontLoader::createFontWithTTFBuffer(std::vector<unsigned char> &buffer, float size)
+{
+    stbtt_fontinfo font;
+    FontData data;
+
+    int fontAtlasW = 512;
+    int fontAtlasH = 512;
+
+    int offset = stbtt_GetFontOffsetForIndex(buffer.data(), 0);
+    if (offset < 0 || !stbtt_InitFont(&font, buffer.data(), offset))
+    {
+        puts("[ERROR] Arquivo corrompido ou formato incompatível");
+        return data;
+    }
+
+    std::vector<unsigned char> bitmap(fontAtlasW * fontAtlasH, 0);
+    data.bitmaps.resize(96 - 32);
+
+    stbtt_BakeFontBitmap(
+        buffer.data(), offset,
+        size,
+        bitmap.data(), fontAtlasW, fontAtlasH,
+        32, 96,
+        data.cdata.data()
+        //
+    );
+
+    data.bitmaps = std::move(bitmap);
+    data.bitmapW = fontAtlasW;
+    data.bitmapH = fontAtlasH;
+    data.pixelH = size;
 
     return data;
 }
